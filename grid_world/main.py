af3459151688f5746b735c04c96c4dedc250bc31
vikrams169 <vikramsetty169@gmail.com>
2024-05-02 00:07:14 -0400

    got LDV working. Need to optimize the code and add better test cases

--- grid_world/main.py
+++ grid_world/main.py
@@ -36,6 +36,9 @@ RED = (255,0,0)
 BLUE = (0,0,255)
 ORANGE = (255,164.5,0)
 
+TARGET_REACHED = False
+LDV_PROB = 0.8
+
 # A class to define the characteristics of each grid cell (generalized to each discrete data point in a robotic configuration space)
 class Node:
 
@@ -48,6 +51,9 @@ class Node:
 		self.visibility = 0.0
 		self.start_node = start_node
 		self.target_node = target_node
+		self.best_path_cost = 1e10
+		#self.best_goal_parent = None
+		self.all_goal_parents = []
 		if self.start_node:
 			self.cost = 0
 
@@ -107,6 +113,33 @@ def is_corner_node(viz_window,point):
 	else:
 		return False
 
+def random_sample():
+	return (random.random()*WINDOW_LENGTH,random.random()*WINDOW_BREADTH)
+
+def LDV_sample(corner_node_list):
+	importances = []
+	for node in corner_node_list:
+		importances.append(node.visibility)
+	importances = np.array(importances)
+	importances = importances/importances.sum()
+	LDV_sample = corner_node_list[int(np.random.choice(importances.shape[0],1,True,importances)[0])]
+	theta = math.atan2(LDV_sample.y-LDV_sample.parent.y,LDV_sample.x-LDV_sample.parent.x)
+	new_pos = [LDV_sample.x+EPSILON*math.cos(theta),LDV_sample.y+EPSILON*math.sin(theta)]
+	new_pos[0] += np.random.normal(EPSILON/2,EPSILON/2)
+	new_pos[1] += np.random.normal(EPSILON/2,EPSILON/2)
+	new_pos[0] = max(min(new_pos[0],WINDOW_LENGTH-1),0)
+	new_pos[1] = max(min(new_pos[1],WINDOW_BREADTH-1),0)
+	return (new_pos[0],new_pos[1])
+
+def sample_point(corner_node_list):
+	if not TARGET_REACHED:
+		return random_sample()
+	else:
+		if np.random.uniform(0,1) > LDV_PROB:
+			return random_sample()
+		else:
+			return LDV_sample(corner_node_list)
+
 # Recursively updating the costs of all the child nodes when a parent node gets rewired
 def update_children(node):
 	for child_node in node.children:
@@ -131,7 +164,7 @@ def find_proximal_node(new_node,node_list):
 
 def get_node_visibility(viz_window,node,theta,step=3):
 	visibility = 0.0
-	current = node.x, node.y
+	current = [node.x,node.y]
 	while True:
 		if current[0] < 0 or current[1] < 0 or current[0] >= WINDOW_LENGTH or current[1] >= WINDOW_BREADTH or in_obstacle(viz_window,(current[0],current[1])):
 			return visibility
@@ -141,6 +174,8 @@ def get_node_visibility(viz_window,node,theta,step=3):
 
 # Rewiring the nodes in the vicinity of the newly added node
 def rewire_nodes(new_node,node_list):
+	if new_node.target_node:
+		return
 	for node in node_list:
 		dist = math.sqrt((new_node.x-node.x)**2 + (new_node.y-node.y)**2)
 		if dist < REWIRING_RADIUS:
@@ -157,7 +192,8 @@ def rewire_nodes(new_node,node_list):
 # Adding a new node while making sure it doesn't collide with any obstacles
 def add_new_node(viz_window,node_list,corner_node_list,obstacle_list):
 	while True:
-		point = (random.random()*WINDOW_LENGTH,random.random()*WINDOW_BREADTH)
+		#point = (random.random()*WINDOW_LENGTH,random.random()*WINDOW_BREADTH)
+		point = sample_point(corner_node_list)
 		nearest_node_dist = 1e7
 		nearest_node = None
 		for node in node_list:
@@ -202,18 +238,45 @@ def display_final_path(viz_window,goal_node):
 	#pygame.image.save(viz_window,dir_name+"/frame"+str(frame_number)+".jpg")
 	frame_number += 1
 
+def display_all_paths(viz_window,goal_node):
+	global frame_number
+	#for parent_node in goal_node.all_goal_parents:
+	#	pygame.draw.line(viz_window,RED,(goal_node.x,goal_node.y),(parent_node.x,parent_node.y),width=5)
+	#	current_node = parent_node
+	#	while not current_node.start_node:
+	#		pygame.draw.line(viz_window,RED,(current_node.x,current_node.y),(current_node.parent.x,current_node.parent.y),width=5)
+	#		current_node = current_node.parent
+	pygame.draw.line(viz_window,RED,(goal_node.x,goal_node.y),(goal_node.parent.x,goal_node.parent.y),width=5)
+	current_node = goal_node.parent
+	while not current_node.start_node:
+		pygame.draw.line(viz_window,RED,(current_node.x,current_node.y),(current_node.parent.x,current_node.parent.y),width=5)
+		current_node = current_node.parent
+	pygame.display.update()
+	#pygame.image.save(viz_window,dir_name+"/frame"+str(frame_number)+".jpg")
+	frame_number += 1
+
 # The RRT* Algorithm
 def rrt_algorithm(viz_window,start_node,goal_node,obstacle_list):
+	global TARGET_REACHED
 	node_list = []
 	corner_node_list = []
 	node_list.append(start_node)
+	i = 0
 	while True:
 		new_node, node_list, corner_node_list = add_new_node(viz_window,node_list,corner_node_list,obstacle_list)
 		if target_reached(new_node,goal_node):
-			goal_node.parent = new_node
+			TARGET_REACHED = True
+			goal_node.all_goal_parents.append(new_node)
+			path_cost = (goal_node.x - new_node.x)**2 + (goal_node.y - new_node.y)**2 + new_node.cost
+			if path_cost < goal_node.best_path_cost:
+				goal_node.best_path_cost = path_cost
+				goal_node.parent = new_node
 			pygame.draw.line(viz_window,BLUE,(new_node.x,new_node.y),(new_node.parent.x,new_node.parent.y))
-			node_list.append(goal_node)
-			display_final_path(viz_window,goal_node)
+			if i == 0:
+				node_list.append(goal_node)
+				i += 1
+			display_all_paths(viz_window,goal_node)
+			#display_final_path(viz_window,goal_node)
 
 # Initializing the grid world as a pygame display window,
 pygame.display.set_caption('RRT* Path Finding Algorithm Visualization')
